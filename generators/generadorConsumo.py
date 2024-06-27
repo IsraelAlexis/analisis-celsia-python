4650de59ec4df76a198c61088865fc0e1e56c92a
Israel Alexis Alzate Tabares <iaalzate@sura.com.co>
2024-06-27 15:36:16 -0500

    creando analisis celsia

---
+++ generators/generadorConsumo.py
@@ -0,0 +1,17 @@
+import random
+
+def generarDatos():
+    datos =[]
+    for i in range(5000):
+        dato={}
+        id=random.randint(1,10000)
+        referencia=random.choice(['ACH01','ACH22','ACH43'])
+        marca=random.choice(['sony','rico','kalley'])
+        capacidad=random.randint(100,2000)
+        ciudad=random.choice(['itagui','medellin','jardin','pintada',''])
+        responsable=random.choice(['santiago posada','mauro yepes','camila builes','andres gallego','israel alzate'])
+
+        dato=[id,referencia,marca,capacidad,ciudad,responsable]
+        datos.append(dato)
+    return datos
+print(generarDatos())
