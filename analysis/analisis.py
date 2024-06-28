83cabec2f5228d73b142df66bf73bc2d0033e832
Israel Alexis Alzate Tabares <iaalzate@sura.com.co>
2024-06-28 16:27:12 -0500

    creando dato consumo

--- analysis/analisis.py
+++ analysis/analisis.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def analizarDatos():
     datos=generarDatos()
-    tabla=pd.DataFrame(datos,columns=['id','referencia','marca','capacidad','ciudad','responsable'])
+    tabla=pd.DataFrame(datos,columns=['id','referencia','marca','capacidad','ciudad','consumo','responsable'])
     tabla.to_csv("./data/archivo.csv", index=False)
 
 analizarDatos()
--- generators/generadorConsumo.py
+++ generators/generadorConsumo.py
@@ -8,10 +8,11 @@ def generarDatos():
         referencia=random.choice(['ACH01','ACH22','ACH43'])
         marca=random.choice(['sony','rico','kalley','-'])
         capacidad=random.randint(100,2000)
+        consumo=random.randint(100,2000)
         ciudad=random.choice(['itagui','medellin','jardin','pintada','envigado','sin','nan'])
         responsable=random.choice(['santiago posada','mauro yepes','camila builes','andres gallego','israel alzate'])
 
-        dato=[id,referencia,marca,capacidad,ciudad,responsable]
+        dato=[id,referencia,marca,capacidad,consumo,ciudad,responsable]
         datos.append(dato)
     return datos
 print(generarDatos())
